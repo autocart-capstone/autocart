fbd257277406bc87b12e8cd785ef7c4f43277349
Nadia Ahmed <nadia.nw.ahmed@gmail.com>
2024-03-05 21:52:33 -0500

    obtain (angle, distance) pairs every 1 sec for 0.2 s

--- MainDevelopment/MappingSystem/Raspberry Pi/simplesocket.py
+++ MainDevelopment/MappingSystem/Raspberry Pi/simplesocket.py
@@ -4,15 +4,7 @@ import smbus
 import time
 
 PORT, HOST_IP = 8080, '127.0.0.1'
-key = 5
 
-channel = 1
-address = 0x12
-
-bus = smbus.SMBus(channel)
-
-currentAngle = 0
-lowestDistance = -1
 angle_distance_pairs = []  # Initialize an empty list to store angle-distance pairs
 
 
@@ -31,34 +23,36 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             d = conn.recv(12)
             values = struct.unpack('ffi', d)
             
+            # If data not received
             if not d:
                 print()
             else:
                 
                 current_time = time.time()
                 elapsed_time = current_time - start_time
-                if(elapsed_time >= 0.2):
-                    #send Data
-                    print("distance ", lowestDistance)
-                    print("angle ", round(currentAngle))
 
-                    msb = (round(currentAngle) >> 8) & 0xFF
-                    lsb = (round(currentAngle)) & 0xFF
-                    
-                    # store angle-distance pairs as a tuple
-                    angle_distance_pairs.append((values[0], values[1]))
-                    
-                    data_to_send = (msb,lsb, round(lowestDistance/10))
-                    data = bytes(data_to_send)
-                    print(list(data))
-                    bus.write_i2c_block_data(address, 0, list(data))
-                    
-                    start_time = time.time()
-                    lowestDistance = -1
-                    
-                if((values[1] < 1000) and (values[1] < lowestDistance or lowestDistance == -1)):
-                    lowestDistance = values[1]
-                    currentAngle = values[0]
+                if(elapsed_time >= 1.0):
+                   measure_start = time.time()
+                   measuring_time = 0
+
+                    while (measuring_time <= 0.2):
+                        d = conn.recv(12)
+                        values = struct.unpack('ffi', d)
+
+                        distance = values[1]
+                        angle = round(values[0])
+                        angle_distance_pairs.append((angle, distance))
+
+                        current_time = time.time()
+                        measuring_time = current_time - measure_start
                 
+                else:
+                    print(angle_distance_pairs)
+                    angle_distance_pairs = []
+                    print{"------------"}
+
+                    
+                start_time = time.time()
+       
                 
                 
