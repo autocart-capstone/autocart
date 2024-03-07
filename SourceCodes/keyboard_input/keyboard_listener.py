a01b49cbfb6d24c6ffd919ddad08c0ffa7083d04
Sebastien Marleau <smarleau.2019@gmail.com>
2024-03-07 14:25:15 -0500

    yes

--- SourceCodes/keyboard_input/keyboard_listener.py
+++ SourceCodes/keyboard_input/keyboard_listener.py
@@ -1,4 +1,10 @@
 import socket
+import struct
+import smbus
+
+channel = 1
+address = 0x12
+bus = smbus.SMBus(channel)
 
 port = 31337
 
@@ -14,14 +20,26 @@ while True:
             msg = data.decode()
             split = msg.split()
             pressed = split[0] == "pressed"
+            if not pressed:
+                bus.write_i2c_block_data(address, 0, list(int.to_bytes(0)))
+                continue
+            
             key = split[1] # Key.up/down/left/right
             #print(f"{key} pressed:{pressed}")
+            # 1 = left
+            # 2 = right
+            # 3 = forward
+            # 4 = back
+            # 0 = stop
             if "up" in key:
                 print(f"up pressed:{pressed}")
+                bus.write_i2c_block_data(address, 0, list(int.to_bytes(3)))
             if "down" in key:
                 print(f"down pressed:{pressed}")
+                bus.write_i2c_block_data(address, 0, list(int.to_bytes(4)))
             if "right" in key:
                 print(f"right pressed:{pressed}")
+                bus.write_i2c_block_data(address, 0, list(int.to_bytes(2)))
             if "left" in key:
                 print(f"left pressed:{pressed}")
-
+                bus.write_i2c_block_data(address, 0, list(int.to_bytes(1)))
