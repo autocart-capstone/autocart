4da37c33093a01b140c1a667f18dedaaa6b0e5fa
shoebe <smarleau.2019@gmail.com>
2024-03-15 20:08:04 -0400

    Fix: actually stop keyboard_listener.py when the bash script exits

--- MainDevelopment/MappingSystem/Raspberry Pi/simplesocket_v2.py
+++ MainDevelopment/MappingSystem/Raspberry Pi/simplesocket_v2.py
@@ -7,6 +7,7 @@ Make sure matlab is done processing and ready to grab new data values
 Send buffer contents (data: angle, distance) to matlab socket connection
 
 """
+
 import socket
 import struct
 import os
@@ -16,42 +17,50 @@ import time
 MATLAB_PORT = 8001
 if "MATLAB_PORT" in os.environ:
     MATLAB_PORT = int(os.environ["MATLAB_PORT"])
-    print(f"Using matlab port: {MATLAB_PORT}")
+print(f"will listen for matlab on port: {MATLAB_PORT}")
 
-class SimpleSocketRpi():
+
+class SimpleSocketRpi:
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.m = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.PORT = 8080
-        self.HOST_IP = '127.0.0.1'
+        self.HOST_IP = "127.0.0.1"
         self.buf = []
 
     def connect_socket(self):
-        self.m.bind(('0.0.0.0', MATLAB_PORT))
+        self.m.bind(("0.0.0.0", MATLAB_PORT))
         self.m.listen(1)
-        
+
         print("starting to listen to matlab")
         conn, addr = self.m.accept()
-        print('Connected by', addr)
+        print("Connected by", addr)
         self.matlab_conn = conn
         self.matlab_conn.setblocking(False)
-        
-        
+
         self.s.bind((self.HOST_IP, self.PORT))
         self.s.listen(1)
         print("starting to listen to sdk")
 
         print("executing sdk")
-        self.sdk_subp = subprocess.Popen(["../rplidar_sdk-master/output/Linux/Release/ultra_simple", "--channel", "--serial", "/dev/ttyUSB0", "115200"], stdout=subprocess.DEVNULL)
-        #os.system("../rplidar_sdk-master/output/Linux/Release/ultra_simple  --channel --serial /dev/ttyUSB0 115200")
+        self.sdk_subp = subprocess.Popen(
+            [
+                "../rplidar_sdk-master/output/Linux/Release/ultra_simple",
+                "--channel",
+                "--serial",
+                "/dev/ttyUSB0",
+                "115200",
+            ],
+            stdout=subprocess.DEVNULL,
+        )
+        # os.system("../rplidar_sdk-master/output/Linux/Release/ultra_simple  --channel --serial /dev/ttyUSB0 115200")
         conn, addr = self.s.accept()
-        print('Connected by', addr)
+        print("Connected by", addr)
         self.sdk_conn = conn
 
-
     def send_data_to_matlab(self):
-        self.matlab_conn.sendall(struct.pack('I', len(self.buf)))
-        self.matlab_conn.sendall(struct.pack(f'{len(self.buf)}f', *self.buf))
+        self.matlab_conn.sendall(struct.pack("I", len(self.buf)))
+        self.matlab_conn.sendall(struct.pack(f"{len(self.buf)}f", *self.buf))
 
     def receive_data_from_matlab(self):
         try:
@@ -60,7 +69,7 @@ class SimpleSocketRpi():
             return b""
 
     def receive_data_from_lidar(self):
-        d = self.sdk_conn.recv(8) #reads up to 8 bytes of data
+        d = self.sdk_conn.recv(8)  # reads up to 8 bytes of data
         if not d:
             print("Recevied empty string")
         # angle, distance = struct.unpack('ff', d)
@@ -75,6 +84,7 @@ class SimpleSocketRpi():
     def reset_buffer(self):
         self.buf = []
 
+
 def main():
     ss = SimpleSocketRpi()
     ss.connect_socket()
@@ -85,12 +95,12 @@ def main():
 
     while True:
         data = ss.receive_data_from_lidar()
-        data = values = struct.unpack('ff', data)
+        data = struct.unpack("ff", data)
 
-        curr_angle = data[0]    # Assuming first elem is angle
+        curr_angle = data[0]  # Assuming first elem is angle
 
         ss.save_data_to_buffer(data)
-        
+
         matlab_pos = ss.receive_data_from_matlab()
         if matlab_pos:
             matlab_ready = True
@@ -98,27 +108,29 @@ def main():
             x = float(a[0])
             y = float(a[1])
             print(f"received (x,y): ({x},{y})")
-            print(f"took {time.time() - matlab_sent_timestamp} seconds to get position from Matlab")
+            print(
+                f"took {time.time() - matlab_sent_timestamp} seconds to get position from Matlab"
+            )
 
-        if curr_angle < prev_angle: # If we finish one revolution
+        if curr_angle < prev_angle:  # If we finish one revolution
             if matlab_ready:
-                #Send buffer to matlab socket
-                #print(ss.get_buffer())
+                # Send buffer to matlab socket
+                # print(ss.get_buffer())
                 print("sent data to Matlab")
                 ss.send_data_to_matlab()
                 matlab_sent_timestamp = time.time()
                 matlab_ready = False
 
-            #Reset buffer - always do this after finishing 1 rev
+            # Reset buffer - always do this after finishing 1 rev
             ss.reset_buffer()
-            #Save first data from the new rev
+            # Save first data from the new rev
             ss.save_data_to_buffer(data)
-        
-        #If we are not done with one rev yet, keep saving data (angle,dist) to buffer
 
-        #Update prev_angle
+        # If we are not done with one rev yet, keep saving data (angle,dist) to buffer
+
+        # Update prev_angle
         prev_angle = curr_angle
-    
+
     # Dummy data - solely for testing
     curr_angle = 0
     # prev_angle = curr_angle
@@ -132,7 +144,6 @@ def main():
     #     print(ss.get_buffer())
     #     prev_angle = curr_angle
     return 0
-
 
 
 main()
--- MainDevelopment/MappingSystem/Raspberry Pi/start.sh
+++ MainDevelopment/MappingSystem/Raspberry Pi/start.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+## Kill background processe(s) on exit (rn only keyboard_listener.py)
+# From https://stackoverflow.com/a/22644006
+trap "exit" INT TERM
+trap "kill 0" EXIT
+
 python3 keyboard_listener.py &
 python3 simplesocket_v2.py
