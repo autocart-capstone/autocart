359b008f5ff543555190f1489936450e1bd9b877
shoebe <smarleau.2019@gmail.com>
2024-04-11 01:06:57 -0400

    forgot testdata file for matlab

---
+++ useful-files/robot-demo/TestData.m
@@ -0,0 +1,145 @@
+tic
+structures = {
+    [00.00,00.11;-0.13,00.11; -0.13,00.28; -0.24,00.28; -0.24,02.06; -0.11,02.06;
+    -0.11,02.24; 00.00,02.24; 02.56,02.24; 02.56,02.37; 03.44,02.37; 03.44,02.24;
+    03.96,02.24; 03.96,06.69; 03.84,06.69; 03.84,07.57; 03.96,07.57; 03.96,12.02; 
+    03.84,12.02; 03.84,12.90; 03.96,12.90; 03.96,14.34; 03.84,14.34; 03.84,15.22; 
+    03.96,15.22; 03.96,16.75; 03.84,16.75; 03.84,17.63; 03.96,17.63; 03.96,17.73;
+    03.88,17.73; 03.88,17.89; 03.96,17.89; 03.96,20.19; 05.07,20.19; 05.07,20.27;
+    05.23,20.27; 05.23,20.19; 05.79,20.19; 05.79,19.25; 16.31,19.25; 16.31,20.00;
+    17.42,20.00; 17.42,20.08; 17.58,20.08; 17.58,20.00; 17.63,20.00; 17.63,19.25;
+    24.52,19.25; 24.52,20.78; 24.02,20.78; 24.02,23.45; 18.96,23.45; 18.96,23.34;
+    18.08,23.34; 18.08,23.45; 17.63,23.45; 17.63,20.38; 17.55,20.38; 17.55,20.11; 
+    17.38,20.11; 17.38,20.38; 17.33,20.38; 17.33,20.11; 16.35,20.11; 16.35,20.38;
+    16.31,20.38; 16.31,23.45; 16.01,23.45; 16.01,23.34; 15.13,23.34; 15.13,23.45; 
+    12.53,23.45; 12.53,23.34; 11.65,23.34; 11.65,23.45; 08.65,23.45; 08.65,23.67; 
+    08.54,23.67; 08.54,24.65; 08.65,24.65; 08.65,25.51; 08.73,25.51; 08.73,25.62; 
+    09.61,25.62; 09.61,25.51; 11.72,25.51; 11.72,25.62; 12.60,25.62; 12.60,25.51;
+    13.24,25.51; 13.24,24.90; 14.45,24.90; 14.45,25.51; 14.65,25.51; 14.65,25.62; 
+    15.53,25.62; 15.53,25.51; 17.51,25.51; 17.51,25.62; 18.39,25.62; 18.39,25.51;
+    18.89,25.51; 18.89,25.62; 19.77,25.62; 19.77,25.51; 23.22,25.51; 23.22,25.62;
+    24.10,25.62; 24.10,25.51; 24.60,25.51; 24.60,25.62; 25.48,25.62; 25.48,25.51;
+    28.93,25.51; 28.93,25.62; 29.81,25.62; 29.81,25.51; 30.01,25.51; 30.01,23.51;
+    25.86,23.51; 25.86,19.25; 26.66,19.25; 26.66,19.36; 27.54,19.36; 27.54,19.25;
+    27.77,19.25; 27.77,19.61; 28.39,19.61; 28.38,19.25; 33.59,19.25; 33.59,16.82;
+    25.97,16.82; 25.97,06.00; 26.08,06.00; 26.08,05.80; 25.97,05.80; 25.97,05.72;
+    26.08,05.72; 26.08,04.84; 25.97,04.84; 25.97,00.11; 25.97,00.11; 25.87,00.11;
+    25.87,00.00; 24.99,00.00; 24.99,00.11; 24.89,00.11; 24.89,00.00; 24.31,00.00;
+    24.31,00.11; 21.26,00.11; 21.26,00.00; 20.38,00.00; 20.38,00.11; 17.50,00.11; 
+    17.50,00.00; 16.62,00.00; 16.62,00.11; 13.78,00.11; 13.78,00.00; 12.90,00.00; 
+    12.90,00.11; 10.24,00.11; 10.24,00.00; 09.36,00.00; 09.36,00.11; 07.15,00.11;
+    07.15,00.00; 06.27,00.00; 06.27,00.11; 04.04,00.11; 04.04,00.00; 03.16,00.00; 
+    03.16,00.11; 01.00,00.11; 01.00,00.00; 00.12,00.00; 00.12,00.11; 00.00,00.11];
+
+    [05.81,01.94;05.81,03.54; 05.94,03.54; 05.94,04.50; 05.81,04.50; 05.81,04.63;
+    05.94,04.63; 05.94,05.59; 05.81,05.59; 05.81,05.72; 05.94,05.72; 05.94,06.68;
+    05.81,06.68; 05.81,07.40; 05.94,07.40; 05.94,08.36; 05.81,08.36; 05.81,08.49;
+    05.94,08.49; 05.94,09.45; 05.81,09.45; 05.81,10.18; 05.94,10.18; 05.94,11.14;
+    05.81,11.14; 05.81,11.27; 05.94,11.27; 05.94,12.23; 05.81,12.23; 05.81,12.36;
+    05.94,12.36; 05.94,13.32; 05.81,13.32; 05.81,14.29; 06.55,14.29; 06.55,15.31;
+    05.81,15.31; 05.81,17.51; 13.86,17.51; 13.86,16.74; 14.86,16.74; 14.86,17.51;
+    24.14,17.51; 24.14,15.95; 23.39,15.95; 23.39,15.90; 23.31,15.90; 23.31,15.74;
+    23.39,15.74; 23.39,14.64; 24.14,14.64; 24.14,01.94; 22.64,01.94; 22.64,02.69;
+    21.62,02.69; 21.62,01.94; 17.79,01.94; 17.79,02.05; 16.09,02.05; 16.09,01.94;
+    15.02,01.94; 15.02,02.07; 14.14,02.07; 14.14,01.94; 14.04,01.94; 14.04,02.07;
+    13.91,02.07; 13.91,01.94; 05.81, 01.94];
+    % [17.15, 22.10; 23.56, 22.10; 23.56, 19.44; 24.00, 19.44; 24.00, 17.68;
+    % 17.15, 17.68; 17.15, 22.10]
+};
+segs = [ structures{1}(1:end-1,:) structures{1}(2:end,:) ; structures{2}(1:end-1,:) structures{2}(2:end,:)];
+test_pos = makeTestGrid(0.1, structures);
+[measures,walls] = makeTestData(test_pos, segs);
+save('ahmed.mat','measures','walls','test_pos','structures');
+toc
+function test_pos = makeTestGrid(space, structures)
+    %%% Make the test grid
+    %     space: separation between points in the test grid (in metres)
+
+    % Make grid of potential test points at specified grid spacing
+    max_x =max(cellfun(@(x) max(x(:,1)), structures));
+    max_y = max(cellfun(@(y) max(y(:,2)), structures));
+    % min_x =min(cellfun(@(x) min(x(:,1)), structures));
+    % min_y =min(cellfun(@(y) min(y(:,1)), structures));
+
+    xv = 0.1:space:max_x;
+    yv = 0.1:space:max_y;
+    xall = reshape(ones(length(yv),1)*xv, [], 1);%%converting 2 dimension array to 1dimension* Multiply 1s length of yv by xv so you just xv for yv times, which is the same as xv
+    yall = reshape(yv'*ones(1,length(xv)), [], 1);
+
+    % Find valid test points (must be in a hallway)
+    in1 = inpolygon(xall, yall, structures{1}(:,1), structures{1}(:,2));
+    in2 = inpolygon(xall, yall, structures{2}(:,1), structures{2}(:,2));
+    good = (in1 & ~in2);
+
+    % Keep only valid test points
+    test_pos = [ xall(good) yall(good) ];
+end
+
+
+function [measures,walls] = makeTestData(test_pos, segs)
+    %%% Generate the expected lidar output at each test position
+    N_testpos = size(test_pos, 1);
+
+    measures = zeros(2, 36, N_testpos);
+    walls = zeros(4, N_testpos);
+    for np = 1:N_testpos
+        xt = test_pos(np,1);    % test position x-value
+        yt = test_pos(np,2);    % test position y-value
+
+        measures(:,:,np) = getLidarMeasures(xt, yt, segs);
+
+        walls(1,np) = measures(1, 1,np) + xt; % right wall
+        walls(2,np) = measures(2,10,np) + yt; % top wall
+        walls(3,np) = measures(1,19,np) + xt; % left wall
+        walls(4,np) = measures(2,28,np) + yt; % bottom wall
+    end
+end
+
+function measures = getLidarMeasures(xs, ys, segs)
+    % Get xy-coordinates of the point where each lidar beam hits an object
+    angle_deg = 0:10:359;
+    angle_rad = deg2rad(angle_deg);
+
+    % Find the distance to the nearest intersection point with the structures
+    d = myintersect(xs,ys, cos(angle_rad),sin(angle_rad), segs);
+    measures = [ d.*cos(angle_rad) ; ...
+                 d.*sin(angle_rad) ];
+end
+
+function d = myintersect(xs,ys, dx,dy, segs)
+    % Find distance to where line from (xs,ys) in direction (dx,dy)
+    % intersects the line segment from p1=(x1,y1) to p2=(x2,y2)
+    % Returns infinity if they do not intersect.
+
+    % Shift origin and shape into column vectors
+    x1 = reshape(segs(:,1), [], 1) - xs;
+    y1 = reshape(segs(:,2), [], 1) - ys;
+    x2 = reshape(segs(:,3), [], 1) - xs;
+    y2 = reshape(segs(:,4), [], 1) - ys;
+
+    % Convert beam directions into row vectors
+    dx = reshape(dx, 1, []); %%ROW VECTOR [dx1 dx2 dx3..... dxN]
+    dy = reshape(dy, 1, []); %%ROW VECTOR [dy1 dy2 dy3..... dyN]
+
+    % Calculate projections
+    d1 = x1.*dx + y1.*dy;       % distance along beam to closest point to p1 %%multiply column by row
+    d2 = x2.*dx + y2.*dy;       % distance along beam to closest point to p2
+
+    % Calculate projection error
+    b1 = y1.*dx - x1.*dy;       % distance from beam to p1
+    b2 = y2.*dx - x2.*dy;       % distance from beam to p2
+
+    % Calculate distance to intersection point
+    r = abs(b2) ./ (abs(b1) + abs(b2));
+    d = d2 + r.*(d1-d2);
+    d(b1.*b2 > 0) = inf;        % beam misses if errors have same sign
+    d(d < 0) = inf;             % beam misses if distance is negative
+
+    % Find nearest intersection point for each beam
+    d = min(d);
+    %disp(d);
+end
+
+
+
+
